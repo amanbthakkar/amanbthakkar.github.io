ee6891af2a32ef4a122c8c854c3e9a2fb1b0429e
Aman Thakkar <aman.thakkar@somaiya.edu>
2023-10-07 12:27:13 -0700

    added url source tracking

--- src/App.js
+++ src/App.js
@@ -24,13 +24,17 @@ function App() {
 
       if (hasCookie) {
         const response = await fetch(
-          'https://cloud.amanthakkar.com/api/old-visitor'
+          `https://cloud.amanthakkar.com/api/old-visitor`
         );
         const data = await response.json();
         setVisitorCount(data.count);
       } else {
+        // grab the source from the url, if any
+        const urlParams = new URLSearchParams(window.location.search);
+        const sourceParam = urlParams.get('source');
+
         const response = await fetch(
-          'https://cloud.amanthakkar.com/api/new-visitor'
+          `https://cloud.amanthakkar.com/api/new-visitor/?source=${sourceParam}`
         );
         const data = await response.json();
         setVisitorCount(data.count);
